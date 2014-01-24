41a052a2f94a40dce2e7efafe8a2593904b5dcc3
Rodrigo Lopes <rlc2@cin.ufpe.br>
2014-01-23 22:22:37 -0300

    speak.sh: comment fix

--- speak.sh
+++ speak.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# write an English text string as an audio file using Google Translate
-# usage: en2audio.sh <text>
+# outputs text through your speakers using Google Translate
+# usage: speak.sh [-l LANG] <text>
+# LANG is en (american english) by default
+
 lang="en"
 if [[ $1 = "-l" ]]; then
 	lang=$2
 	shift 2
 fi
-echo "speaking in $lang"
+echo "speaking $@ in $lang"
 echo $@ > >(wget -q -U Mozilla "http://translate.google.com/translate_tts?ie=UTF-8&tl=$lang&q=$*" -O - | mplayer -cache 8192 - 2>1 > /dev/null)
