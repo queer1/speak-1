aa299cd4eb6d8c51a912ab92ff247c0200332119
Rodrigo Lopes <rodrigolpscarvalho@gmail.com>
2014-01-24 12:58:03 -0300

    Fixed output redirection causing a file named "1" being created.

--- speak.sh
+++ speak.sh
@@ -11,4 +11,4 @@ if [[ $1 = "-l" ]]; then
 	shift 2
 fi
 echo "speaking $@ in $lang"
-echo $@ > >(wget -q -U Mozilla "http://translate.google.com/translate_tts?ie=UTF-8&tl=$lang&q=$*" -O - | mplayer -cache 8192 - 2>1 > /dev/null)
+echo $@ > >(wget -q -U Mozilla "http://translate.google.com/translate_tts?ie=UTF-8&tl=$lang&q=$*" -O - | mplayer -cache 8192 - > /dev/null 2>&1)
